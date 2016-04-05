a405d65b7dd908296bace8a198b54cffdeef9a2a
Sooraj Chandran <soorajchn@gmail.com>
2016-04-06 01:14:59 +0530

    Version 1.0

--- zoompanzoom.js
+++ zoompanzoom.js
@@ -1,46 +1,47 @@
+(function($) {
 
-(function ( $ ) {
- 
-   $.fn.zoompanzoom = function(options) {
+    $.fn.zoompanzoom = function(options) {
 
-   	var settings = $.extend({
+        var settings = $.extend({
             animationSpeed: "fast",
-            zoomfactor:.1,
-            maxZoom:3,
-            minZoom:0.5,
-            disablepan:false   
-        }, options );
+            zoomfactor: .1,
+            maxZoom: 3,
+            minZoom: 0.5,
+            disablePan: false
+        }, options);
 
-	var zoomdiv  = this;
-	currentZoom = 1.0;
+        var zoomdiv = this;
+        currentZoom = 1.0;
 
-	if(!settings.disablepan)
-	{
-		$(this).draggable();
-	}
-	
+        if (!settings.disablePan) {
+            $(this).draggable();
+        }
 
-	jQuery('#zoom_in').click(
-		function () {
-			if(currentZoom<settings.maxZoom)
-			{
-				jQuery(zoomdiv).animate({ 'zoom': currentZoom += settings.zoomfactor }, settings.animationSpeed);
-			}
-			
-		})
-	jQuery('#zoom_out').click(
-		function () {
-			if(currentZoom>settings.minZoom)
-			{
-				jQuery(zoomdiv).animate({ 'zoom': currentZoom -= settings.zoomfactor }, settings.animationSpeed);
-			}
-			
-		})
-	jQuery('#zoom_reset').click(
-		function () {
-			currentZoom = 1.0;
-			jQuery(zoomdiv).animate({ 'zoom': 1 }, settings.animationSpeed);
-		})
-};
- 
-}( jQuery ));
+        jQuery('#zoom_in').click(
+            function() {
+                if (currentZoom < settings.maxZoom) {
+                    jQuery(zoomdiv).animate({
+                        'zoom': currentZoom += settings.zoomfactor
+                    }, settings.animationSpeed);
+                }
+
+            })
+        jQuery('#zoom_out').click(
+            function() {
+                if (currentZoom > settings.minZoom) {
+                    jQuery(zoomdiv).animate({
+                        'zoom': currentZoom -= settings.zoomfactor
+                    }, settings.animationSpeed);
+                }
+
+            })
+        jQuery('#zoom_reset').click(
+            function() {
+                currentZoom = 1.0;
+                jQuery(zoomdiv).animate({
+                    'zoom': 1
+                }, settings.animationSpeed);
+            })
+    };
+
+}(jQuery));
